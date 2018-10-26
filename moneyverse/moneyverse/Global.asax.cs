9bed0bf7b0e4542a7c2b981622e0e664adafc13d
Michael Mourao <michaelmourao@hotmail.com>
2018-10-26 21:34:51 +0300

    second commit

---
+++ moneyverse/moneyverse/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+namespace moneyverse
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(config =>
+            {
+                config.MapHttpAttributeRoutes();
+
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            });
+        }
+    }
+}
